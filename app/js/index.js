2d6abab9a407d882fb163a40114922b305ad817c
Alessio Massetti <alessiomassetti@gmail.com>
2019-07-06 14:59:04 +0200

    Fullscreen + optimization

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    const feeds = [];
     $.getJSON("config/config.json", function (json) {
         if (json.feeds) {
             json.feeds.forEach((feed, index) => {
@@ -9,7 +10,16 @@ $(document).ready(() => {
                 $('.screens').append(html);
                 const openbar = `<button data-open="${index}">${index}</button>`;
                 $('.openbar').append(openbar);
-            })
+                feeds.push({
+                    id: index,
+                    feed: feed,
+                    open: true
+                })
+            });
+            const openFeeds = feeds.filter((o) => {
+                return o.open
+            }).length;
+            $('.container').css('flex-basis', 200 / (openFeeds + 1) + "%");
         }
         const buttonSelector = $('button');
         const barSelector = $('.openbar');
@@ -23,13 +33,53 @@ $(document).ready(() => {
                 $('.flex').find('[data-id=' + close + ']').hide();
                 buttonSelector.find('[data-close=' + close + ']').hide();
                 barSelector.find('[data-open=' + close + ']').show();
+                feeds.find((o) => {
+                    return o.id === Number(close)
+                }).open = false;
             }
             if (open) {
                 $('.flex').find('[data-id=' + open + ']').show();
                 buttonSelector.find('[data-close=' + open + ']').show();
                 barSelector.find('[data-open=' + open + ']').hide();
+                feeds.find((o) => {
+                    return o.id === Number(open)
+                }).open = true;
+            }
+            const openFeeds = feeds.filter((o) => {
+                return o.open
+            }).length;
+            $('.container').css('flex-basis', 200 / (openFeeds + 1) + "%");
+        });
+        $('#fullScreenButton').click(() => {
+            $('#fullScreenButton').hide();
+            let elem = document.getElementById('body');
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen().then(()=> {
+                    $('#fullScreenButton').hide();
+                });
+            } else if (elem.mozRequestFullScreen) { /* Firefox */
+                elem.mozRequestFullScreen().then(()=> {
+                    $('#fullScreenButton').hide();
+                });
+            } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+                elem.webkitRequestFullscreen().then(()=> {
+                    $('#fullScreenButton').hide();
+                });
+            } else if (elem.msRequestFullscreen) { /* IE/Edge */
+                elem.msRequestFullscreen().then(()=> {
+                    $('#fullScreenButton').hide();
+                });
             }
         });
+        document.addEventListener("fullscreenchange", function () {
+            $('#fullScreenButton').show();
+        }, false);
+        document.addEventListener("webkitfullscreenchange", function () {
+            $('#fullScreenButton').show();
+        }, false);
+        document.addEventListener("mozfullscreenchange", function () {
+            $('#fullScreenButton').show();
+        }, false);
     });
 
 });
